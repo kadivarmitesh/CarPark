ace5b44ec3236b2400e847996276cff96c19cb21
kadivarmitesh <mitesh.kadivar@overseasits.com>
2019-05-10 16:11:01 +0530

    add dot net project on github

---
+++ CarPark/Controllers/CarParkController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using CarPark.Models;
+
+namespace CarPark.Controllers
+{
+    public class CarParkController : Controller
+    {
+        List<CarParkData> lst = new List<CarParkData>();
+        public async Task<ActionResult> Index()
+        {
+            Uri uri = new System.Uri("https://sps-opendata.pilotsmartke.gov.hk/rest/getCarparkInfos");
+
+            HttpClient _httpClient = new HttpClient();
+            var response = await _httpClient.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+            var data = await response.Content.ReadAsStringAsync();
+            CarParkData carParkData =  JsonConvert.DeserializeObject<CarParkData>(data);
+            
+            return View(carParkData);
+        }
+
+
+        
+        private static JsonSerializerSettings MicrosoftDateFormatSettings
+        {
+            get
+            {
+                return new JsonSerializerSettings
+                {
+                    DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+                };
+            }
+        }
+    }
+}
---
+++ CarPark/Models/CarPark.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarPark.Models
+{
+    
+        public class OpeningHour
+        {
+            public List<string> weekdays { get; set; }
+            public bool excludePublicHoliday { get; set; }
+            public string periodStart { get; set; }
+            public string periodEnd { get; set; }
+        }
+
+        public class MotorCycle
+        {
+            public int space { get; set; }
+        }
+
+        public class Coach
+        {
+            public int space { get; set; }
+        }
+
+        public class HGV
+        {
+            public int space { get; set; }
+        }
+
+        public class LGV
+        {
+            public int space { get; set; }
+        }
+
+        public class PrivateCar
+        {
+            public int space { get; set; }
+        }
+
+        public class RenditionUrls
+        {
+            public string square { get; set; }
+            public string thumbnail { get; set; }
+            public string banner { get; set; }
+        }
+
+        public class Result
+        {
+            public int _id { get; set; }
+            public string name { get; set; }
+            public string nature { get; set; }
+            public string type { get; set; }
+            public List<OpeningHour> openingHours { get; set; }
+            public MotorCycle motorCycle { get; set; }
+            public Coach coach { get; set; }
+            public HGV HGV { get; set; }
+            public LGV LGV { get; set; }
+            public PrivateCar privateCar { get; set; }
+            public List<double> coordinates { get; set; }
+            public RenditionUrls renditionUrls { get; set; }
+        }
+
+        public class CarParkData
+        {
+            public List<Result> results { get; set; }
+        }
+    
+}
